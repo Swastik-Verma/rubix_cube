5a34cd7aff1aa546e0e68e5b13db0077f3c16484
Swastik-Verma <swastikjee09@gmail.com>
2025-06-23 10:24:24 +0530

    added generic rubix cube model

---
+++ generic_rubix_cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lenovo on 6/23/2025.
+//
+
+#include "generic_rubix_cube.h"
---
+++ generic_rubix_cube.h
@@ -0,0 +1,16 @@
+//
+// Created by Lenovo on 6/23/2025.
+//
+
+#ifndef GENERIC_RUBIX_CUBE_H
+#define GENERIC_RUBIX_CUBE_H
+
+
+
+class generic_rubix_cube {
+
+};
+
+
+
+#endif //GENERIC_RUBIX_CUBE_H
